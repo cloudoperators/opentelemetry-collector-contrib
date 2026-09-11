03f801a59eee5b78e9579877f07cb7539a3b22d3
Simon Olander <simon.olander@sap.com>
2026-09-11 15:06:45 +0200

    fix(exporter/opensearchexporter): fixes hardcoded index value to use bulkIndexer
    ---------
    
    Signed-off-by: Simon Olander <simon.olander@sap.com>

--- exporter/opensearchexporter/log_bulk_indexer.go
+++ exporter/opensearchexporter/log_bulk_indexer.go
@@ -215,7 +215,7 @@ func (lbi *logBulkIndexer) flushOnErrorIndex(ctx context.Context, client *opense
 	for _, doc := range lbi.onErrorDocs {
 		doc := doc
 		item := opensearchutil.BulkIndexerItem{
-			Action: "index",
+			Action: lbi.bulkAction,
 			Index:  lbi.onErrorIndex,
 			Body:   bytes.NewReader(doc),
 		}
